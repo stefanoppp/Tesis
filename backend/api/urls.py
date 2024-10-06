e2bce02c909a9fc4bc762efe610db9d25d6e2d2a
Stefano <spstefanoperetti17@gmail.com>
2024-10-05 21:28:57 -0300

    recepcion bd

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import RegisterView, LoginView, MainPageView
+from .views import UploadFileView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('main/', MainPageView.as_view(), name='main'),
+    path('upload/', UploadFileView.as_view(), name='upload-file'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
-
+from django.core.files.storage import FileSystemStorage
 # Registro
 class RegisterView(APIView):
     permission_classes = [AllowAny]
@@ -38,9 +38,35 @@ class LoginView(APIView):
             }, status=status.HTTP_200_OK)
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
-# Main page protegida
+# Vistas protegidas
 class MainPageView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
         return Response({"message": f"Ingresado exitosamente! Usuario, {request.user.username}!"}, status=status.HTTP_200_OK)
+
+class UploadFileView(APIView):
+    permission_classes = [IsAuthenticated] 
+    def post(self, request):
+        # Obtén el archivo y los parámetros numéricos
+        file = request.FILES.get('file')
+        param1 = request.data.get('param1')
+        param2 = request.data.get('param2')
+
+        # Verifica si el archivo y los parámetros están presentes
+        if not file:
+            return Response({'error': 'No file provided.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if param1 is None or param2 is None:
+            return Response({'error': 'Both param1 and param2 must be provided.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Aquí puedes agregar validaciones para los parámetros numéricos si es necesario
+
+        # Guarda el archivo (puedes usar almacenamiento en S3)
+        fs = FileSystemStorage()
+        filename = fs.save(file.name, file)
+        
+        # Llama a tu proceso de IA aquí, pasando los parámetros
+        # process_data(filename, param1, param2)
+
+        return Response({'message': 'File uploaded successfully', 'filename': filename, 'param1': param1, 'param2': param2}, status=status.HTTP_201_CREATED)
